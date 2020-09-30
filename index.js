b33ad8eba3f6659f13cce5befd5a20aa9038a6cd
Theelipan prabakar <theelipan@gmail.com>
2020-09-29 23:55:45 -0400

    show data by colume

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require("dotenv").config({ path: "./credentials/.env" });
 const SPREADSHEETID = process.env.SHEETID;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CLIENT_EMAIL = process.env.CLIENT_EMAIL;
-console.log("CLIENT_EMAIL", CLIENT_EMAIL);
 const { google } = require("googleapis");
 
 const client = new google.auth.JWT(CLIENT_EMAIL, null, PRIVATE_KEY, ["https://www.googleapis.com/auth/spreadsheets.readonly"]);
@@ -22,10 +21,23 @@ async function gsrun(cl) {
 
   const options = {
     spreadsheetId: SPREADSHEETID,
-    range: "Sheet1!A1:F8",
+    range: "Sheet1!A2:F8",
+    majorDimension: "COLUMNS",
   };
 
   let data = await gsapi.spreadsheets.values.get(options);
-  console.log(data.data.values[1][0]);
-  console.log(data.data.values[1][1]);
+  projectContent(data);
+}
+
+function projectContent(data) {
+  console.clear();
+
+  data.data.values.forEach((el) => {
+    console.log(`Title: ${el[0]}`);
+    console.log(`Desc: ${el[1]}`);
+    console.log(`Tools: ${el[2]}`);
+    console.log(`Demo: ${el[3]}`);
+    console.log(`Git: ${el[4]}`);
+    console.log(`---------------------------------------------------------------`);
+  });
 }
